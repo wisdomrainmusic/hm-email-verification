7cb9f8f9e674ed4da294a5f841dbfc1823026da1
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2026-01-04 10:34:19 +0300

    Align shortcode bootstrap loading

--- hm-email-verification/includes/bootstrap.php
+++ hm-email-verification/includes/bootstrap.php
@@ -4,20 +4,28 @@ if (!defined('ABSPATH')) { exit; }
 if (!function_exists('hm_ev_bootstrap')) {
     function hm_ev_bootstrap() {
 
-        // Lang (Commit 3)
+        // Lang
         $lang = HM_EV_PATH . 'includes/class-hm-ev-lang.php';
         if (file_exists($lang)) require_once $lang;
 
-        // I18n (Commit 4)
+        // I18n
         $i18n = HM_EV_PATH . 'includes/class-hm-ev-i18n.php';
         if (file_exists($i18n)) require_once $i18n;
 
-        // Core (Commit 2)
+        // Core
         $core = HM_EV_PATH . 'includes/class-hm-ev-core.php';
         if (file_exists($core)) require_once $core;
 
+        // Shortcode (Commit 5)
+        $sc = HM_EV_PATH . 'includes/class-hm-ev-shortcode.php';
+        if (file_exists($sc)) require_once $sc;
+
         if (class_exists('HM_EV_Core')) {
             HM_EV_Core::init();
+        }
+
+        if (class_exists('HM_EV_Shortcode')) {
+            HM_EV_Shortcode::init();
         }
     }
 }
